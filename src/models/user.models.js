bae2342d0c0fdf1e0dbfe4e8bfec152a218971c9
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2024-12-19 03:13:59 +0530

    jwt debugged

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -32,7 +32,7 @@ userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.method.generateRefreshToken = function(){
+userSchema.method.generateAccessToken = function(){
     return jwt.sign({
         _id: this._id,
         email: this.email,
@@ -41,7 +41,7 @@ userSchema.method.generateRefreshToken = function(){
     }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: process.env.ACCESS_TOKEN_EXPIRY})
 }
 
-userSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign({
         _id: this._id
     }, process.env.REFRESH_TOKEN_SECRET, {expiresIn: process.env.REFRESH_TOKEN_EXPIRY})
